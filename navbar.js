45957959eb645164fcb8f64f7d04d6ebb20eab6f
shriman-rajesh <shriman.rajesh@outlook.com>
2026-05-21 11:56:22 -0400

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -1,8 +1,8 @@
 document.write(`
 <ul>
-  <li><a href="index.html" id="home">Home</a></li>
-  <li><a href="resume.html" id="res">Resume</a></li>
-  <li><a href="education.html" id="edu">Education</a></li>
-  <li><a href="cad_portfolio.html" id="cadp">CAD Portfolio</a></li>
+  <li><a href="./" id="home">Home</a></li>
+  <li><a href="./resume" id="res">Resume</a></li>
+  <li><a href="./education" id="edu">Education</a></li>
+  <li><a href="./cad-portfolio" id="cadp">CAD Portfolio</a></li>
 </ul>
 `);
